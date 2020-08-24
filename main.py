6b74fcfc9f5f82be99e5720ec0e6d6fa6182d107
ATREMA\tlint <tlint@deloitte.at>
2020-08-24 15:55:26 +0200

    add Copyright

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 """
-@author: Tobias Lint
-@email: tobias@lint.at
+Copyright (c) 2020 Tobias Lint <tobias@lint.at>. All rights reserved.
+Licensed under the MIT License.
 """
 from Generators.DataBaseGenerator import DataBaseGenerator
 import os
